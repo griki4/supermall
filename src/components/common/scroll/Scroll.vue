663fb1642ead59047d6fed723d7b7db2fe684f18
griki4 <15215066996@163.com>
2022-07-17 18:28:08 +0800

    首页滚动模块开发

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,66 @@
+<template>
+<!--  滚动条组件，负责轮home-swipper recommend feature组件的滚动-->
+  <div ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll"
+export default {
+  name: "Scroll",
+  props:{
+    probeType:{
+      type:Number,
+      default:0
+    },
+    pullUpLoad:{
+      type:Boolean,
+      default: false
+    }
+  },
+  data(){
+    return {
+      scroll:null
+    }
+  },
+  mounted() {
+    //1.创建BScroll滚动对象，赋值给scroll变量
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click:true,//控制类似div之类的元素是否可以被点击
+      probeType:this.probeType,
+      pullUpLoad:this.pullUpLoad
+    })
+
+    //2.监听滚动的位置
+    if(this.probeType===2 || this.probeType===3){
+      this.scroll.on('scroll', position => {
+        // console.log(position)
+        this.$emit('scroll', position)
+      })
+    }
+
+    //3.监听是否滚动到底部
+    if (this.pullUpLoad === true) {
+      this.scroll.on('pullingUp', ()=>{
+        this.$emit('pullingUp')
+      })
+    }
+  },
+  methods:{
+    scrollTo(x, y, time=300) {
+      this.scroll && this.scroll.scrollTo(x,y,time)
+    },
+    finishPullUp(){
+      this.scroll.finishPullUp()
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,115 @@
 <template>
-  <h2>分类</h2>
+<!--  <div class="wipper">
+      <ul class="list">
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+        <li>商品分类</li>
+      </ul>
+  </div>-->
 </template>
 
 <script>
+//让移动端使用滚动功能的时候更加顺滑
+/*import BScroll from "better-scroll"
   export default {
-    name: "Category"
-  }
+    name: "Category",
+    data(){
+      return {
+        scroll: null
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll('.wipper',{
+      })
+    }
+  }*/
 </script>
 
 <style scoped>
-
+/*  .wipper {
+    height: 150px;
+    background-color: skyblue;
+    overflow: hidden;
+  }*/
 </style>
