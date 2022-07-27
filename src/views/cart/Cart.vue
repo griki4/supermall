daf45d48666ec565d4c3691c5c2ae79110fc139e
griki4 <15215066996@163.com>
2022-07-27 11:34:05 +0800

    购物车界面导航栏和商品列表组件开发

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,13 +1,37 @@
 <template>
-  <h2>购物车</h2>
+  <div id="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{ cartLength }})</div>
+    </nav-bar>
+    <cart-list></cart-list>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Cart"
+import NavBar from "@/components/common/navbar/NavBar";
+import CartList from "@/views/cart/childComponents/CartList";
+
+import {mapGetters} from 'vuex'
+
+export default {
+  name: "Cart",
+  components: {
+    NavBar,
+    CartList
+  },
+  computed: {
+    ...mapGetters(['cartLength'])
   }
+}
 </script>
 
 <style scoped>
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 
+#cart {
+  height: 100vh;
+}
 </style>
---
+++ src/views/cart/childComponents/CartListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <div>{{ itemInfo }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartListItem",
+  props: {
+    itemInfo: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
