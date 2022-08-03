9cf3e1dd979f031aef60a78641f26be60d41fd00
griki4 <15215066996@163.com>
2022-08-03 14:49:49 +0800

    个人信息界面开发

---
+++ src/views/profile/ChildComponents/TopView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "TopView"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,13 +1,26 @@
 <template>
-  <h2>个人</h2>
+<div class="profile">
+  <nav-bar class="pro-nav">
+    <div slot="center">个人中心</div>
+  </nav-bar>
+</div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import TopView from "@/views/profile/ChildComponents/TopView";
   export default {
-    name: "Profile"
+    name: "Profile",
+    components:{
+      NavBar,
+      TopView
+    }
   }
 </script>
 
 <style scoped>
-
+.pro-nav {
+  background-color: var(--color-tint);
+  color: #f6f6f6;
+}
 </style>
